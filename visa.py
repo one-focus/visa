8547991ae23352d317d476bcaf40ec48f912a9dd
sozdai <sash.kardash@gmail.com>
2020-12-09 00:20:48 +0300

    add headless

--- visa.py
+++ visa.py
@@ -30,7 +30,7 @@ def send_screenshot():
 
 def get_dates():
     chrome_options = webdriver.ChromeOptions()
-    # chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--window-size=1024,768")
     chrome_options.add_argument("--no-sandbox")
