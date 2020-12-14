7c91171e8747d5b580aa7b974a9af5f6dc1fd355
sozdai <sash.kardash@gmail.com>
2020-12-14 05:47:55 +0300

    updated to take times

--- bot.py
+++ bot.py
@@ -1,12 +1,20 @@
 import telebot
 import visa
+from selenium import webdriver
 
 bot = telebot.TeleBot("1461082086:AAGUnZJyEcDwkW1LPHLmezbrXEDzIu6nD8k")
 
-dates, screenshot = visa.get_dates()
-if dates:
-    bot.send_message(chat_id=262438, text=dates)
-    bot.send_photo(chat_id=262438, photo=screenshot)
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--window-size=1024,768")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(chrome_options=chrome_options)
+driver.implicitly_wait(10)
+
+times, screenshot = visa.get_times(driver)
+if not times:
+    driver.quit()
 else:
-    bot.send_message(chat_id=262438, text="no dates")
+    bot.send_message(chat_id=262438, text=times)
     bot.send_photo(chat_id=262438, photo=screenshot)
--- visa.py
+++ visa.py
@@ -12,31 +12,7 @@ URL = 'http://www.exteriores.gob.es/Embajadas/NUEVADELHI/en/Noticias/Pages/Artic
 IS_MONITORING = False
 
 
-def send_screenshot():
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    # chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--window-size=1024,768")
-    chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-    driver.implicitly_wait(120)
-    try:
-        driver.find_element_by_class_name('popup-appCloseIcon').click()
-    except Exception:
-        pass
-    return driver.get_screenshot_as_png()
-
-
-def get_dates():
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--window-size=1024,768")
-    chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(chrome_options=chrome_options)
-    driver.implicitly_wait(10)
-
+def get_times(driver):
     try:
         print('2')
         driver.get(URL)
@@ -61,23 +37,18 @@ def get_dates():
         print('11')
         WebDriverWait(driver, 60).until(
             EC.invisibility_of_element_located((By.XPATH, '//div[@class="clsDivBktWidgetDefaultLoading"]')))
-        print('12')
-        WebDriverWait(driver, 60).until(
-            EC.element_to_be_clickable((By.ID, 'idDivBktDatetimeDatePickerIcon'))).click()
-        print('13')
         time.sleep(3)
-        elements = driver.find_elements_by_xpath("//table[@class='ui-datepicker-calendar']//td")
-        print('14')
-        time.sleep(3)
-        dates = []
-        if len(elements):
-            for element in elements:
-                if element.get_attribute("title") not in ('', 'CLOSED', 'NOT AVAILABLE'):
-                    dates.append(f'{element.text}:{element.get_attribute("title")}')
+        if driver.find_element_by_id('idDivNotAvailableSlotsTextTop').is_displayed():
+            times = []
+        else:
+            times_elements = driver.find_elements_by_id("clsDivDatetimeSlot")
+            times = []
+            print(14)
+            if times_elements:
+                for time_slot in times_elements:
+                    times.append(time_slot.text.strip())
+        print(times)
     except Exception as e:
-        dates = str(e)
+        times = str(e)
     screen = driver.get_screenshot_as_png()
-    driver.quit()
-    return dates, screen
-
-
+    return times, screen
