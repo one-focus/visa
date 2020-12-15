ece89543a9680c21ac489d990422fc2f0da8c2d2
sozdai <sash.kardash@gmail.com>
2020-12-15 11:37:32 +0300

    update reg

--- bot.py
+++ bot.py
@@ -15,31 +15,38 @@ chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(chrome_options=chrome_options)
 driver.implicitly_wait(10)
 
+def create_user(driver):
+    try:
+        driver.find_element_by_xpath("//div[@class='clsDivDatetimeSlot']/..").click()
+
+        user = {'passport': '11106422',
+                'passport_expired': '22/09/2028',
+                'name': 'Jajwalish',
+                'surname': 'Raios',
+                'email': 'agafinovaguzel@gmail.com',
+                'cellphone': 'rega11rega11@mail.ru',
+                'nationality': 'Nepal',
+                'create_pass': 'Abcd123456!'}
+        visa.create_user(driver, user['passport'], user['passport_expired'], user['name'], user['surname'], user['email'],
+                         user['cellphone'], user['nationality'], user['create_pass'])
+
+        bot.send_photo(chat_id=int(sys.argv[3]), photo=driver.get_screenshot_as_png())
+        time.sleep(10)
+        bot.send_photo(chat_id=int(sys.argv[3]), photo=driver.get_screenshot_as_png())
+    except Exception as e:
+        bot.send_message(chat_id=int(sys.argv[3]), text=str(e))
+        bot.send_photo(chat_id=int(sys.argv[3]), photo=driver.get_screenshot_as_png())
+
 time.sleep(int(sys.argv[1]))
 times, screenshot = visa.get_times(driver)
 if times:
     bot.send_message(chat_id=int(sys.argv[3]), text=str(times))
     bot.send_photo(chat_id=int(sys.argv[3]), photo=screenshot)
+    create_user(driver)
+else:
+    import datetime
+    bot.send_message(chat_id=int(sys.argv[3]), text=f'no dates:{datetime.datetime.now()}')
 
-try:
-    driver.find_element_by_xpath("//div[@class='clsDivDatetimeSlot']/..").click()
-
-    user = {'passport': '11106422',
-            'passport_expired': '22/09/2028',
-            'name': 'Jajwalish',
-            'surname': 'Raios',
-            'email': 'agafinovaguzel@gmail.com',
-            'cellphone': 'rega11rega11@mail.ru',
-            'nationality': 'Nepal',
-            'create_pass': 'Abcd123456!'}
-    visa.create_user(driver, user['passport'], user['passport_expired'], user['name'], user['surname'], user['email'],
-                     user['cellphone'], user['nationality'], user['create_pass'])
-
-    bot.send_photo(chat_id=int(sys.argv[3]), photo=driver.get_screenshot_as_png())
-    time.sleep(10)
-    bot.send_photo(chat_id=int(sys.argv[3]), photo=driver.get_screenshot_as_png())
-except Exception as e:
-    bot.send_message(chat_id=int(sys.argv[3]), text=str(e))
-    bot.send_photo(chat_id=int(sys.argv[3]), photo=driver.get_screenshot_as_png())
 
 driver.quit()
+
