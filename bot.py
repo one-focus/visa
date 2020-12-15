4e17cef432ed32a1a322b9c285763f25aedf54ab
sozdai <sash.kardash@gmail.com>
2020-12-15 11:26:29 +0300

    update reg

--- bot.py
+++ bot.py
@@ -21,22 +21,25 @@ if times:
     bot.send_message(chat_id=int(sys.argv[3]), text=str(times))
     bot.send_photo(chat_id=int(sys.argv[3]), photo=screenshot)
 
-driver.find_element_by_xpath("//div[@class='clsDivDatetimeSlot']/..").click()
+try:
+    driver.find_element_by_xpath("//div[@class='clsDivDatetimeSlot']/..").click()
 
-user = {'passport': '11106422',
-        'passport_expired': '22/09/2028',
-        'name': 'Jajwalish',
-        'surname': 'Raios',
-        'email': 'agafinovaguzel@gmail.com',
-        'cellphone': 'rega11rega11@mail.ru',
-        'nationality': 'Nepal',
-        'create_pass': 'Abcd123456!'}
-visa.create_user(driver, user['passport'], user['passport_expired'], user['name'], user['surname'], user['email'],
-                 user['cellphone'], user['nationality'], user['create_pass'])
+    user = {'passport': '11106422',
+            'passport_expired': '22/09/2028',
+            'name': 'Jajwalish',
+            'surname': 'Raios',
+            'email': 'agafinovaguzel@gmail.com',
+            'cellphone': 'rega11rega11@mail.ru',
+            'nationality': 'Nepal',
+            'create_pass': 'Abcd123456!'}
+    visa.create_user(driver, user['passport'], user['passport_expired'], user['name'], user['surname'], user['email'],
+                     user['cellphone'], user['nationality'], user['create_pass'])
 
-time.sleep(2)
-bot.send_photo(chat_id=int(sys.argv[3]), photo=driver.get_screenshot_as_png())
-time.sleep(10)
-bot.send_photo(chat_id=int(sys.argv[3]), photo=driver.get_screenshot_as_png())
+    bot.send_photo(chat_id=int(sys.argv[3]), photo=driver.get_screenshot_as_png())
+    time.sleep(10)
+    bot.send_photo(chat_id=int(sys.argv[3]), photo=driver.get_screenshot_as_png())
+except Exception as e:
+    bot.send_message(chat_id=int(sys.argv[3]), text=str(e))
+    bot.send_photo(chat_id=int(sys.argv[3]), photo=driver.get_screenshot_as_png())
 
 driver.quit()
