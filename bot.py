e8ba5cadaa6904c3510fc40278fcf35ee63ad4f5
sozdai <sash.kardash@gmail.com>
2020-12-26 00:39:23 +0300

    update timer to 200 sec

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ import sys
 bot = telebot.TeleBot(sys.argv[2])
 
 chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_argument("--headless")
+chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--window-size=1024,768")
 chrome_options.add_argument("--no-sandbox")
