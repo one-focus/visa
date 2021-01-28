2d363f651e53575f9e3d62bd02159801a9c2e59c
sozdai <sash.kardash@gmail.com>
2021-01-28 13:39:44 +0300

    run

--- bot.py
+++ bot.py
@@ -8,11 +8,11 @@ import sys
 bot = telebot.TeleBot(sys.argv[2])
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--window-size=1024,768")
 chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(chrome_options=chrome_options)
+driver = webdriver.Chrome(options=chrome_options)
 driver.implicitly_wait(1)
 
 
